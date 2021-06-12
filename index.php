b0b19904e6731a099b18de009fc8fbf2b888a4d8
Lukasz Tukindorf <lukasz@codeaddinc.io>
2021-06-12 13:35:26 +0200

    sreate second section

--- index.php
+++ index.php
@@ -9,7 +9,33 @@
 </head>
 <body>
     <?php @include './components/navbar.php' ?>
-    <div style="height: 30vh"/>
+    <main class="homePage">
+        <section class="topSection">
+            <img src="assets/img/logo-tarcza.svg" alt="logo" class="topSection__logo" >
+            <h1 class="topSection__title">
+                Title placeholder two lines
+            </h1>
+            <p class="topSection__text">
+            Kancelarię stanowi zespół wykwalifikowanych prawników posiadających doświadczenie w obsłudze prawnej dużych projektów na całym świecie.
+            </p>
+            <button class="button">O NAS</button>
+        </section>
+        <section class="secondSection">
+            <div class="secondSection__leftColumn">
+                <h2 class="secondSection__title">
+                    Placeholder for title
+                </h2>
+                <img 
+                    src="assets/img/secondSection.png"
+                    alt="img"
+                    class="secondSection__leftColumn__img"
+                >
+            </div>
+            <div class="secondSection__rightColumn">
+                
+            </div>
+        </section>
+    </main>
     <?php @include './components/footer.php' ?> 
 </body>
 </html>
